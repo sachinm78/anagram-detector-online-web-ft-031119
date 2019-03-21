8464a8bf57a9f15d217efe84097c2c50bd2fb801
Sachin Mahajan <sachinm78@gmail.com>
2019-03-21 17:43:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,12 +7,13 @@ class Anagram
     @word = word
   end
   
-  def match(word_list) 
-    return_array = []
-    #array_of_words.each do |element|
-      #(@word.split("").sort) == (element.split("").sort)
-        #return_array << element
-      #end
-    #end
-    #return_array
+  def match(word_list)
+    return_list = []
+    word_list.each do |anagram|
+      if anagram.split("").sort == @word.split("").sort
+        return_list << anagram
+      end
+    end
+    return_list
+  end
 end
